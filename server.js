ba8602c4841fd13afc6d684ac38b9716cb6fc189
Geetika Guleria <geetikaguleria3@gmail.com>
2018-01-31 13:34:11 +0530

    node web server initial commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+app.set('view engine','hbs');
+
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url} `;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err) {
+      console.log('Unable to log to server.log file');
+    }
+  });
+  next();
+});
+// 
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res) => {
+  res.render('index.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to our home page!'
+  });
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page'
+  });
+});
+
+app.get('/bad',(req,res) => {
+  res.send({
+    errorMsg: 'Bad request'
+  });
+});
+app.listen(3000);
